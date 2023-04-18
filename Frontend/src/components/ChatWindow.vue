27adfcdbde60a1121a4f682839c01910c1b1f839
alexwallden <alexander.wallden@me.com>
2023-04-18 13:11:33 +0200

    added chatSocket.ts

--- Frontend/src/components/ChatWindow.vue
+++ Frontend/src/components/ChatWindow.vue
@@ -3,18 +3,22 @@
     <div class="chat-messages"></div>
     <input class="chat-input" id="inputText" type="text" />
     <button class="chat-input-btn" @click="sendMessage">Send</button>
+    <ul>
+      <li v-for="(message, i) in chatState.chatMessages" :key="i">
+        {{ message.messageBody }}
+      </li>
+    </ul>
   </section>
 </template>
 
 <script setup lang="ts">
-  import {socket} from '../socket';
-  import { ref } from "vue";
-
+  import { chatSocket } from '@/sockets/chatSocket';
+  import { chatState } from '@/sockets/chatSocket';
   
   function sendMessage() {
     const message = (document.getElementById("inputText") as HTMLInputElement).value;
     
-    socket.emit("chat", message)
+    chatSocket.emit("chat", {messageBody: message})
   }
   
 </script>
