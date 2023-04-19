9c0d890f89b1a2fa975dd32ec4a877d276ff4b32
Jesper Eriksson <84853548+jeppeerixon@users.noreply.github.com>
2023-04-19 10:34:41 +0200

    added way of showing color

--- Frontend/src/components/PlayerInfo.vue
+++ Frontend/src/components/PlayerInfo.vue
@@ -1,22 +1,26 @@
 <template>
     <div class="player-info-container">
         <h3 class="player-name">{{ playerName }}</h3>
-        <p class="player-color">{{ PlayerColor }}</p>
+        <p class="player-color">color</p>
     </div>
 
 </template>
 
 <script setup lang="ts">
 import { useUserStore } from '@/stores/userStore'
-
+import { ref } from 'vue'
 
 const store = useUserStore()
 
 const playerName = store.users[0].name
 const PlayerColor = store.users[0].color
+const PlayerColorCss = ref(PlayerColor)
 
 </script>
 
-<style>
+<style scoped lang="scss">
+.player-color {
+    color: v-bind(PlayerColorCss);
+}
 
 </style>
