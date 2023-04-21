327ee52481dadbc45a9c13a9c88315ecb30d633f
Jesper Eriksson <84853548+jeppeerixon@users.noreply.github.com>
2023-04-21 11:09:07 +0200

    added timer model and timeSocket

---
+++ Frontend/src/sockets/timerSocket.ts
@@ -0,0 +1,16 @@
+import type Timer from '@/models/Timer';
+import { reactive } from 'vue'
+import { io } from 'socket.io-client'
+// import { socket } from '@/socket';
+
+export const timerState = reactive({
+    time: [] as Timer[]
+})
+
+export const timerSocket = io('http://localhost:3000');
+
+timerSocket.on('time', (time: Timer) => {
+    timerState.time = [];
+    console.log('Mottaget timer');
+    timerState.time.push(time)
+})
