c02ae67878f79a48a6711e024d746ce8fdf74f2b
alexwallden <alexander.wallden@me.com>
2023-04-25 22:04:08 +0200

    nytt försök

--- Frontend/vite.config.ts
+++ Frontend/vite.config.ts
@@ -15,5 +15,26 @@ export default defineConfig({
   build: {
     outDir: './dist',
   },
-  base: '/'
+  base: '/Frontend/dist/'
 })
+
+
+// import { fileURLToPath, URL } from 'node:url'
+
+// import { defineConfig } from 'vite'
+// import vue from '@vitejs/plugin-vue'
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [vue()],
+//   resolve: {
+//     alias: {
+//       '@': fileURLToPath(new URL('./src', import.meta.url))
+//     },
+//   },
+//   root: './',
+//   build: {
+//     outDir: './dist',
+//   },
+//   base: '/Frontend/dist'
+// })
